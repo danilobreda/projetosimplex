3c8d208ff287031cd991d2c5643f2df7f0fde9bd
Jorge Takano Junior <takano.xn@gmail.com>
2015-09-27 16:35:22 -0300

    Tela do Projeto

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<!--<link href="css/arquivo.css" rel="stylesheet">-->
+	<title>Simplex</title>
+	<!--<link rel="shortcut icon" href="imagens\favicon.ico" type="image/x-icon">-->
+</head>
+<body>
+	<table width="800px" align="center">
+		<tr>
+			<td>
+				<fieldset><legend>Função Objetivo</legend>
+					<table>
+						<tr>
+							<th>Max: Z = </th>
+							<td><input type="text" id="funcaoObjetivo" tabindex="1" maxlength="12" size="17"></td>
+						</tr>
+					</table>
+				</fieldset>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>	
+<?php 
+	 
+	
+?>
