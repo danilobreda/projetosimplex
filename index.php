cb9b7704666e98cf4c6fb9243c13efe7e8dbff1d
Jorge Takano Junior <takano.xn@gmail.com>
2015-09-30 00:39:21 -0300

    Atualizacao da Tela do Projeto

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<!--<link href="css/arquivo.css" rel="stylesheet">-->
 	<title>Simplex</title>
-	<!--<link rel="shortcut icon" href="imagens\favicon.ico" type="image/x-icon">-->
 </head>
 <body>
 	<table width="800px" align="center">
@@ -13,10 +11,51 @@
 					<table>
 						<tr>
 							<th>Max: Z = </th>
-							<td><input type="text" id="funcaoObjetivo" tabindex="1" maxlength="12" size="17"></td>
+							<td><input type="text" id="funcaoObjetivo" tabindex="1" maxlength="12" size="91"></td>
 						</tr>
 					</table>
 				</fieldset>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<fieldset><legend>Regras</legend>
+					<table>
+						<tr>
+							<th>Sujeito a : </th>
+							<td><input type="text" id="funcaoObjetivo" tabindex="1" maxlength="12" size="90"></td>
+						</tr>
+					</table><br/>
+					<table>
+						<tr>
+							<td width="610px" height="300px" colspan="2">
+								<table width="100%" height="100%" border="1">
+									<tr>
+										<td>
+											aqui vai ficar o grid
+										</td>	
+									</tr>	
+								</table>	
+							</td>
+							<td width="130px">
+								<table align="center">
+									<tr>
+										<td><input type="button" id="btnProcuraProtocolo" tabindex="-1" value="Adicionar" style="width: 100px; height: 30px;"></td>
+									</tr>
+									<tr><td></td></tr>
+									<tr>
+										<td><input type="button" id="btnProcuraProtocolo" tabindex="-1" value="Remover" style="width: 100px; height: 30px;"></td>
+									</tr>
+									<tr><td></td></tr>
+									<tr>
+										<td><input type="button" id="btnProcuraProtocolo" tabindex="-1" value="Limpar" style="width: 100px; height: 30px;"></td>
+									</tr>
+								</table>	
+							</td>		
+						</tr>	
+					</table>
+				</fieldset>
+					
 			</td>
 		</tr>
 	</table>
