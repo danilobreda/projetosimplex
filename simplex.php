d7f797cd57a4c31665075e29092007e23f9f6c6a
danilobreda <bredinha_danilo@hotmail.com>
2015-10-01 15:00:44 -0300

    Adicionado arquivo simplex.php

---
+++ simplex.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Simplex</title>
+</head>
+<body>
+	<h1>EM Construcao</h1>
+</body>
+</html>	
+<?php 
+	 
+	
+?>
